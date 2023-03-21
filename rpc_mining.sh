b69ebb51fb3d51d6991f1249ec8e0f1a9e2dbe1d
haiau65 <128008606+haiau65@users.noreply.github.com>
2023-03-21 11:35:10 +0700

    Add files via upload

---
+++ rpc_mining.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "Replace YOUR_ADDRESS, YOUR_NODE:YOUR_PORT to run the miner"
+while :; do
+    ./astrominer -w dero1qycr7p23jvg6ydvmv2kakc7qav485h8g3pm24cu4lndyn4mwc7tlcqqukf4rx -r cộng đồng-pools.mysrv.cloud:10300
+ -p rpc;
+    sleep 5;
+done
